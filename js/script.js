8fd51741f80484ba7c26c5e70aaca4dc266e1d57
eatmad ramzy <eatmadramzy197@gmail.com>
2024-12-30 19:19:50 +0200

    creat project

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+let navLinks=document.querySelectorAll(".nav-menu  .nav-link");
+let menuOpenButton=document.querySelector("#menu-open-button");
+let menuCloseButton=document.querySelector("#menu-close-button");
+menuOpenButton.addEventListener("click",()=>{
+    document.body.classList.toggle("show-mobile-menu");
+})
+
+menuCloseButton.addEventListener("click",()=> menuOpenButton.click());
+navLinks.forEach(link=>{
+  link.addEventListener("click",()=>menuOpenButton.click())
+})
+
+
+const swiper = new Swiper('.slider-wrapper', {
+    
+    loop: true,
+    spaceBetween:25,
+    grabCursor:true,
+  
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1
+
+        },
+        768:{
+            slidesPerView:2
+
+        },
+        1024:{
+            slidesPerView:3
+
+        },
+    }
+  
+   
+  });  
